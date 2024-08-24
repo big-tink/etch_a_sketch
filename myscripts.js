5377bcc9dd1f19ba3213ecfa49ad46e852b2d976
Jake Bellm <[jbsnake13@gmail.com]>
2024-08-23 22:24:02 -0500

    Add color and opacity change function

--- myscripts.js
+++ myscripts.js
@@ -1,49 +1,66 @@
-
-
-
-// function generateRows(rows = 16) {
-
-//     let columns = rows
-
-//     for (i = rows;i > 0; i--) {
-//         const container = document.querySelector('#container');
-//         const squaresRow = document.createElement('div');
-//         squaresRow.setAttribute('class', 'row');
-//         squaresRow.setAttribute('style', 'background: #ffffff; display: flex; flex: 1;');
-//         container.appendChild(squaresRow);
-//     }
-       
-//     generateColumns(columns);
-
-// }
-
-// function generateColumns(columns) {
-
-//     for (columns;columns > 0; columns--) {
-//         const squaresRow = document.querySelectorAll('#container.row')
-//         const squaresColumn = document.createElement('div');
-//         squaresColumn.setAttribute('class', 'box')
-//         squaresColumn.setAttribute('style', 'background: #ffffff; display: flex-basis: 0; border: 1px black solid;')
-//         squaresRow.appendChild(squaresColumn);
-//     }
-
-// }
-
-// generateRows(16);
+generateBoard();
+setColor();
 
 function generateBoard(x = 16) {
 
 
     for (squares = x*x;squares > 0; squares--) {
-    const container = document.querySelector('#container');
-    const boxSize = 760 / x;
-    const grid = document.createElement('div')
-    grid.setAttribute('class', 'grid');
-    grid.setAttribute('style', 'background: #ffffff;')
-    grid.style.width = `${boxSize}px`
-    container.appendChild(grid)
+        const container = document.querySelector('#container');
+        const boxSize = 760 / x;
+        const grid = document.createElement('div')
+        grid.setAttribute('class', 'grid');
+        grid.style.width = `${boxSize}px`
+        grid.style.height = `${boxSize}px`
+        container.appendChild(grid)
     }
 
+    setColor();
+
+};
+
+// creates a new game
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+
+    const container = document.querySelector('#container')
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
+    let dimensions = prompt("Please enter a size for the game?")
+
+    if (dimensions > 0 && dimensions < 101) {
+        generateBoard(dimensions);
+    } else {
+        dimensions = prompt("The number must be between 1 and 100.");
+        generateBoard(dimensions);
+    }
+
+});
+
+function setColor() {
+
+const game = document.querySelectorAll('.grid')
+
+for (let i = 0; i < game.length; i++) {
+
+    let opacity = 0;
+
+    game[i].addEventListener("mouseover", () => {
+        let colorRed = Math.floor(Math.random() * 255);
+        let colorGreen = Math.floor(Math.random() * 255);
+        let colorBlue = Math.floor(Math.random() * 255);
+
+        if (opacity < 1) {
+        opacity = opacity + .1;
+        }
+
+        game[i].style.backgroundColor = `rgb(${colorRed}, ${colorGreen}, ${colorBlue}, ${opacity})`;
+        console.log("it works")
+
+    });
 }
 
-generateBoard();
+};
+
